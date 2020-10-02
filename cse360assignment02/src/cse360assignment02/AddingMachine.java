dec12139244e29d8019dac0686312c9eeae10501
Kieup <ktpham7@asu.edu>
2020-10-02 16:24:01 -0700

    Initial version of AddingMachine file

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,28 @@
+package cse360assignment02;
+/*In this assignment,we will implement an AddingMachine classs
+ * In the AddingMachine class, we will use to calculate the total by having getTptal function
+ * add method, subtract method,toString method*/
+public class AddingMachine {
+  private int total;
+  /*create a AddingMachine function */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*the getTotal will return the total value at the current */
+  public int getTotal () {
+    return 0;
+  }
+  /* add function will add the parameter value that pass in the function to the current total */
+  public void add (int value) {
+  }
+  /* subtract function will take the current total and subtract the value passing in the parameter */
+  public void subtract (int value) {
+  }
+/* toString function will return the string of the history calculation */
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
